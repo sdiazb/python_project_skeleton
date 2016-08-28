12b524026f5392cc85dbc150567ed65bffc60e2f
Sergio Díaz Bautista <s.diazbautista@gmail.com>
2016-08-28 17:21:36 +0200

    Managing test with tox and adding integration with travis

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        tox.cmdline(args=args)
 
 version = '0.0.0'
 github_url = 'https://github.com/sdiazb/python_project_skeleton'
@@ -19,8 +38,10 @@ config = {
         'Programming Language :: Python :: 2.7',
     ],
     'install_requires': [],
-    'extras_require': {
-        'test': ['nose', 'coverage']
+    'tests_requires': ['tox', 'nose', 'coverage'],
+    'extras_require': {},
+    'cmdclass': {
+        'test': Tox
     },
     'packages': find_packages(exclude=['docs', 'tests*']),
     'scripts': []
